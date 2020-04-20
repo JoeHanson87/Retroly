8e85126f0930e6ef8318b953f79f031e5c742c06
Joe Hanson <Joe.Hanson87@outlook.com>
2020-04-20 15:38:22 +0100

    Added Customer and Game Controllers

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Retroly.Models;
+
+namespace Retroly.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        List<Customer> customerList = new List<Customer>
+        {
+            new Customer {Id = 1, Name = "Joe Bloggs"},
+            new Customer {Id = 2, Name = "John Smith"},
+            new Customer {Id = 3, Name = "Jane Doe"}
+        };
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(customerList);
+        }
+    }
+}
---
+++ Controllers/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Retroly.Models;
+
+namespace Retroly.Controllers
+{
+    public class GameController : Controller
+    {
+        List<Game> GamesList = new List<Game>
+        {
+            new Game {Id = 1, Name = "Sonic The Hedgehog" },
+            new Game {Id = 2, Name = "Cool Spot"},
+            new Game {Id = 3, Name = "Golden Axe"}
+        };
+
+        // GET: Game
+        public ActionResult Index()
+        {
+            return View(GamesList);
+        }
+    }
+}
