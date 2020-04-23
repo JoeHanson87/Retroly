ca958ce5ae30b1b5c3adf0f6583b41e08f4d012a
Joe Hanson <Joe.Hanson87@outlook.com>
2020-04-23 16:01:52 +0100

    Adding JQuery Stuff

---
+++ Controllers/API/GamesController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Retroly.Models;
+using Retroly.Dtos;
+using AutoMapper;
+
+namespace Retroly.Controllers.API
+{
+    public class GamesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public GamesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        //GET /api/games -- Get the list of all Games
+        public IHttpActionResult GetGames()
+        {
+            return Ok(_context.Games.ToList().Select(Mapper.Map<Game, GameDto>));
+        }
+
+        //GET /api/game/1 -- Get a single Game
+        public IHttpActionResult GetGame(int id)
+        {
+            var game = _context.Games.SingleOrDefault(g => g.Id == id);
+
+            if (game == null)
+                return NotFound();
+
+            return Ok(Mapper.Map<Game, GameDto>(game));
+        }
+
+        //POST /api/games -- Create a Game Entry
+        [HttpPost]
+        public IHttpActionResult CreateGame(GameDto gameDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var game = Mapper.Map<GameDto, Game>(gameDto);
+            _context.Games.Add(game);
+            _context.SaveChanges();
+
+            gameDto.Id = game.Id;
+
+            return Created(new Uri(Request.RequestUri + "/" + game.Id), gameDto);
+        }
+
+        //PUT /api/game/1 -- Update a Customer Entry
+        [HttpPut]
+        public void UpdateGame(int id, GameDto gameDto)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var gameInDb = _context.Games.SingleOrDefault(g => g.Id == id);
+
+            if (gameInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            Mapper.Map(gameDto, gameInDb);
+
+            _context.SaveChanges();
+        }
+
+        // DELETE /api/games/1
+        [HttpDelete]
+        public void DeleteGame(int id)
+        {
+            var gameInDb = _context.Games.SingleOrDefault(g => g.Id == id);
+
+            if (gameInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Games.Remove(gameInDb);
+            _context.SaveChanges();
+        }
+    }
+
+}
